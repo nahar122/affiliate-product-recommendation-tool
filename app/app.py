3dd74fa11547006f29f383d6985e8d2f3d01f0fa
Nahar Saias <nahar2002@gmail.com>
2023-12-20 22:59:34 -0500

    testing injection script and added endpoint to retrieve injected paragraph

--- app/app.py
+++ app/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, render_template, redirect, url_for, session, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
 from helpers.funcs import get_urls_from_csv
 from main import process_articles
 from scrapy.crawler import CrawlerProcess
 from article_crawler.spiders.article_spider import ArticleSpider
 import pathlib
-import crochet
+import os
 from threading import Thread, Event
 import logging
+from db.models import DatabaseManager, URL
+import crochet
 crochet.setup()
 load_dotenv()
 
@@ -17,9 +19,9 @@ PASSWORD = 'admin'
 logging.basicConfig(level=logging.INFO)
 
 # @crochet.run_in_reactor
-def run_spider_with_urls(urls, form, outputfile, finished_event):
+def run_spider_with_urls(urls, outputfile, finished_event):
     settings = {
-        'USER_AGENT': 'your_user_agent',
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
         'FEED_FORMAT': 'json',
         'FEED_URI': outputfile,
         # other settings
@@ -27,36 +29,39 @@ def run_spider_with_urls(urls, form, outputfile, finished_event):
 
     process = CrawlerProcess(settings)
     ArticleSpider.urls_to_scrape = urls
-    # attribute_type = form.get("attributeType")
-    # if attribute_type == "class":
-    #     ArticleSpider.extract_by = "class"
-    # elif attribute_type == "id":
-    #     ArticleSpider.extract_by = "id"
-    # ArticleSpider.title_value = form.get("title")
-    # ArticleSpider.paragraph_value = form.get("paragraph")
-
     logging.info("Spider started")
     deferred = process.crawl(ArticleSpider)
     deferred.addBoth(lambda _: finished_event.set())
     return deferred
-    # deferred = process.start(stop_after_crawl=True)
-    # deferred.addBoth(lambda _: finished_event.set())
-    # finished_event.set()
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    # DB_HOST = os.environ.get("DB_HOST")
-    # DB_NAME = os.environ.get("DB_NAME")
-    # DB_USER = os.environ.get("DB_USER")
-    # DB_PASS = os.environ.get("DB_PASS")
-    # app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
     app.secret_key = "my-secret-key"
-    
-    
     return app
 
 app = create_app()
+
+db_uri = f"mysql+pymysql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASS')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
+db_manager = DatabaseManager(db_uri=db_uri)
+
+@app.route('/retrieve-url-data', methods=['POST'])
+def retrieve_url_data():
+    data = request.get_json()
+    url_to_find = data.get('url')
+
+    # Query the database
+    url_entry = db_manager.db_session.query(URL).filter_by(url=url_to_find).first()
+
+    if url_entry:
+        return jsonify({
+            'url': url_entry.url,
+            'article_title': url_entry.article_title,
+            'initial_article_paragraph': url_entry.initial_article_paragraph,
+            'injected_article_paragraph': url_entry.injected_article_paragraph
+        })
+    else:
+        return jsonify({'message': 'URL not found'}), 404
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -90,7 +95,7 @@ def upload():
             # Run spider and add callback for process_articles
             finished_event = Event()
 
-            Thread(target=run_spider_with_urls, args=(article_urls, request.form, output_path, finished_event)).start()
+            Thread(target=run_spider_with_urls, args=(article_urls, output_path, finished_event)).start()
 
             def wait_and_process():
                 finished_event.wait()
@@ -104,5 +109,4 @@ def upload():
     return render_template('upload.html')
 
 if __name__ == '__main__':
-
     app.run(host="0.0.0.0", port=80, debug=True)
--- injection.js
+++ injection.js
@@ -47,5 +47,49 @@ async function makeRequests(api_key) {
   }
 }
 
-// Call the function with your API key
-makeRequests("test_api_key");
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+async function scrapeWebPage(url) {
+  try {
+    // Fetch the HTML of the page
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+
+    // Extract the first <h1> tag for the title
+    const title = $("h1").first().text();
+
+    // Initialize the first paragraph variable
+    let firstParagraph = null;
+
+    // Check all <p> tags
+    $("p").each((i, elem) => {
+      if (firstParagraph === null) {
+        const paragraphText = $(elem).text();
+        const wordCount = paragraphText.split(" ").length;
+        // If word count is more than 30, set as first paragraph and break
+        if (wordCount > 30) {
+          firstParagraph = paragraphText;
+          return false; // Break the loop
+        }
+      }
+    });
+
+    // If no suitable <p> tag found, get all text after <h1> until the first <br>
+    if (!firstParagraph) {
+      firstParagraph = $("h1").first().nextUntil("br").text();
+    }
+
+    console.log("Title:", title);
+    console.log("First Paragraph:", firstParagraph);
+  } catch (error) {
+    console.error("Error scraping the web page:", error);
+  }
+}
+
+// Usage
+scrapeWebPage(
+  "https://www.osradar.com/how-to-enable-wifi-in-windows-server-2022/"
+);
+
+// makeRequests("test_api_key");
