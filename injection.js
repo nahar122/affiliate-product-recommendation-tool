f510ecab087e8b196d4d6b8124f16dc6e4b29fd4
Nahar Saias <nahar2002@gmail.com>
2023-12-30 14:18:30 -0500

    testing line break in injection

--- injection.js
+++ injection.js
@@ -57,12 +57,7 @@ function isSimilar(text1, text2) {
 function insertLineBreakAfterThreeSentences(htmlString) {
   const parser = new DOMParser();
   const doc = parser.parseFromString(htmlString, "text/html");
-  const walker = document.createTreeWalker(
-    doc.body,
-    NodeFilter.SHOW_TEXT,
-    null,
-    false
-  );
+  const walker = document.createTreeWalker(doc.body, NodeFilter.SHOW_TEXT);
 
   let sentenceCount = 0;
   let sentences = "";
@@ -73,21 +68,13 @@ function insertLineBreakAfterThreeSentences(htmlString) {
     let parts = currentText.split(/([.!?]\s+)/g);
 
     for (let part of parts) {
+      sentences += part;
       if (part.match(/[.!?]/)) {
         sentenceCount++;
-        sentences += part;
         if (sentenceCount >= 3) {
-          let tempDiv = document.createElement("div");
-          tempDiv.innerHTML = sentences;
-          walker.currentNode.parentNode.insertBefore(
-            tempDiv.firstChild,
-            walker.currentNode
-          );
-          sentences = "";
+          sentences += "<br>"; // Adding a line break
           sentenceCount = 0;
         }
-      } else {
-        sentences += part;
       }
     }
     walker.currentNode.textContent = sentences;
@@ -97,6 +84,11 @@ function insertLineBreakAfterThreeSentences(htmlString) {
   return doc.body.innerHTML;
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  scrapeWebPage();
-});
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   scrapeWebPage();
+// });
+
+let x = insertLineBreakAfterThreeSentences(
+  "<p class='' style=''>Learning programming languages is a good option to find a good job. Above all, if you dedicate yourself to this. Because for no one is a secret that we live in times where many applications are used. That’s why this post will teach you how to install <a href='https://www.osradar.com/install-rust-programming-language-ubuntu-debian/'>Rust</a> on Ubuntu 20.04 / Debian 10. If you're looking to dive deep into Rust programming, you might be interested in <a href='https://www.amazon.com/dp/1718503105?&amp;_encoding=UTF8&amp;tag=synth0f-20'>The Rust Programming Language, 2nd Edition</a> or <a href='https://www.amazon.com/dp/1492052590?&amp;_encoding=UTF8&amp;tag=synth0f-20'>Programming Rust: Fast, Safe Systems Development</a>. Additionally, if you want to explore practical examples, consider <a href='https://www.amazon.com/dp/1788390636?&amp;_encoding=UTF8&amp;tag=synth0f-20'>Rust Programming By Example: Enter the world of Rust by building engaging, concurrent, reactive, and robust applications</a> or <a href='https://www.amazon.com/dp/B0742HGLWB?&amp;_encoding=UTF8&amp;tag=synth0f-20'>Rust Essentials: A quick guide to writing fast, safe, and concurrent systems and applications, 2nd Edition</a>. For dealing with rust in a different context, you might find <a href='https://www.amazon.com/dp/B0000AY8PB?&amp;_encoding=UTF8&amp;tag=synth0f-20'>Star brite Rust Eater &amp; Converter - Chemically Converts Rust Into Steel</a> useful.</p>"
+);
+console.log(x);
